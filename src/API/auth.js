1b8abb2c63e75aad0c38278e78079945482ba685
Roman <roman.ustychenko@gmail.com>
2023-06-04 20:49:35 +0300

    Update auth.js

--- src/API/auth.js
+++ src/API/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: 
-    // "https://rest-api-back.onrender.com/api" || 
+    "https://rest-api-back.onrender.com/api" || 
     "http://localhost:3000/api",
     // baseURL: "http://localhost:3000/api",
     
